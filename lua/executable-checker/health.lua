717453ea11bf627968851144e2d5bd7adf1f74fc
Zhe Yu <zcabzyu@ucl.ac.uk>
2024-02-06 03:48:05 +0000

    Switch to new API.

--- lua/executable-checker/health.lua
+++ lua/executable-checker/health.lua
@@ -1,16 +1,16 @@
 local config = require("executable-checker.config")
 
 local okay_func = function(exec_name)
-  vim.health.report_ok(exec_name .. " is found in $PATH.")
+  vim.health.ok(exec_name .. " is found in $PATH.")
 end
 
 local error_func = function(exec_name)
-  vim.health.report_error(exec_name .. " is not installed !!!")
+  vim.health.error(exec_name .. " is not installed !!!")
 end
 
 return {
   check = function()
-    vim.health.report_start("Running executable checker...")
+    vim.health.start("Running executable checker...")
     for _, executable in ipairs(config.executables) do
       if type(executable) == "string" then
         if vim.fn.executable(executable) ~= 0 then
